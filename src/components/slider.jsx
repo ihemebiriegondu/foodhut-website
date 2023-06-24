11762c5f41ce61230b18c30fbd26b7853db1ebf8
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-06-24 23:31:37 +0100

    finished the animations in the header

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -3,11 +3,11 @@ import { IoIosArrowDown } from 'react-icons/io'
 
 export default function Slider() {
     return (
-        <div className='flex flex-col items-center slider'>
-            <p className='bg-secondary rounded-full first-slide w-2.5 h-2.5 mb-1'></p>
-            <p className='bg-secondary rounded-full second-slide w-2 h-2 mb-1'></p>
-            <p className='bg-secondary rounded-full third-slide w-1.5 h-1.5 mb-1'></p>
-            <p className='bg-secondary rounded-full forth-slide w-1 h-1'></p>
+        <div className='flex flex-col items-center animate-bounce'>
+            <p className='bg-secondary rounded-full w-2.5 h-2.5 mb-1'></p>
+            <p className='bg-secondary rounded-full w-2 h-2 mb-1'></p>
+            <p className='bg-secondary rounded-full w-1.5 h-1.5 mb-1'></p>
+            <p className='bg-secondary rounded-full w-1 h-1 -mb-2'></p>
             <IoIosArrowDown className='text-3xl text-primary' />
         </div>
     )
