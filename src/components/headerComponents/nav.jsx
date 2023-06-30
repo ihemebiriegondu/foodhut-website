5391884ce0852b814b6016ca93316f507275ca2a
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-06-30 14:14:05 +0100

    added offcanvas

--- src/components/headerComponents/nav.jsx
+++ src/components/headerComponents/nav.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../assests/little-decors/Logo (1).png'
 import FancyButton from '../fancyButton'
 
 import { LuMenu } from 'react-icons/lu'
+import { IoClose } from 'react-icons/io5'
+import OffCanvas from './offCanvas'
 
 export default function Nav() {
-    return (
-        <nav className='flex justify-between items-center lg:mb-8 relative z-10'>
-            <div className='w-[4.875rem] h-[7.563rem] ms-1.5'>
-                <a href="/"><img src={Logo} alt="food hut logo" className='w-full h-full object-contain' /></a>
-            </div>
 
-            <div className='hidden lg:flex items-center lg:gap-x-12 xlg:gap-x-14 xl:gap-x-24'>
-                <div className='flex lg:gap-x-8 xlg:gap-x-12 xl:gap-x-12'>
-                    <a href="/#todaysOffer">Today special offers</a>
-                    <a href="/">Why FoodHut</a>
-                    <a href="/#ourMenu">Our Menu</a>
-                    <a href="/">Cart</a>
+    const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+    return (
+        <nav>
+            <div className='flex justify-between items-center lg:mb-8 relative z-10'>
+                <div className='w-[4.875rem] h-[7.563rem] ms-1.5'>
+                    <a href="/"><img src={Logo} alt="food hut logo" className='w-full h-full object-contain' /></a>
                 </div>
-                <div>
-                    <FancyButton content={'Download App'} />
+
+                <div className='hidden lg:flex items-center lg:gap-x-12 xlg:gap-x-14 xl:gap-x-24'>
+                    <div className='flex lg:gap-x-8 xlg:gap-x-12 xl:gap-x-12'>
+                        <a href="/#todaysOffer">Today special offers</a>
+                        <a href="/">Why FoodHut</a>
+                        <a href="/#ourMenu">Our Menu</a>
+                        <a href="/">Cart</a>
+                    </div>
+                    <div>
+                        <FancyButton content={'Download App'} />
+                    </div>
+                </div>
+                <div className='block lg:hidden' onClick={() => { setShowOffcanvas(!showOffcanvas) }}>
+                    <LuMenu className={`text-4xl mt-3 ${showOffcanvas ? 'hidden' : 'block'}`} />
+                    <IoClose className={`text-4xl mt-3 ${showOffcanvas ? 'block' : 'hidden'}`} />
                 </div>
             </div>
-            <div className='block lg:hidden pe-6'>
-                <LuMenu className='text-4xl mt-3' />
+            <div className={`absolute right-6 md:right-10 lg:right-12 xlg:right-16 left-6 md:left-10 lg:left-12 xlg:left-16 top-24 z-20 transition-all duration-400 ease ${showOffcanvas ? 'block' : 'hidden'}`}>
+                <OffCanvas />
             </div>
         </nav>
     )
---
+++ src/components/headerComponents/offCanvas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function OffCanvas() {
+    return (
+        <div className='bg-primary px-8 py-5 rounded-lg shadow-lg shadow-primary/30 block lg:hidden'>
+            <ul className='flex flex-col gap-y-3 text-white'>
+                <li><a href="/#todaysOffer">Today special offers</a></li>
+                <li><a href="/">Why FoodHut</a></li>
+                <li><a href="/#ourMenu">Our Menu</a></li>
+                <li><a href="/">Cart</a></li>
+            </ul>
+        </div>
+    )
+}
