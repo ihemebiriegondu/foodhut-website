4040faa1927c721d8af54f9ac1243ed567b82102
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-07-04 21:29:54 +0100

    made nav responsive, added dark mode style to html

--- src/components/dashboardComponents/dashboardnav.jsx
+++ src/components/dashboardComponents/dashboardnav.jsx
@@ -8,20 +8,20 @@ import { BiSolidCart, BiSearch } from 'react-icons/bi'
 export default function Dashboardnav({ dashboardboldFont, menuboldFont }) {
     return (
         <nav>
-            <div className='flex justify-between items-center lg:mb-8 relative z-10'>
-                <div className='w-[4.875rem] h-[7.563rem] ms-1.5 dark:hidden'>
+            <div className='flex justify-between gap-x-3 items-center lg:mb-8 relative z-10'>
+                <div className='w-14 h-28 xsm:w-[4.875rem] xsm:h-[7.563rem] ms-1.5 dark:hidden'>
                     <a href="/dashboard"><img src={Logo} alt="food hut logo" className='w-full h-full object-contain' /></a>
                 </div>
-                <div className='w-[4.875rem] h-[7.563rem] ms-1.5 hidden dark:block'>
+                <div className='w-14 h-28 xsm:w-[4.875rem] xsm:h-[7.563rem] ms-1.5 hidden dark:block'>
                     <a href="/dashboard"><img src={darkLogo} alt="food hut logo" className='w-full h-full object-contain' /></a>
                 </div>
 
-                <div className='flex items-center gap-x-12 xlg:gap-x-14 xl:gap-x-24'>
-                    <div className='flex gap-x-8 xlg:gap-x-12 xl:gap-x-12 dark:text-white text-lg'>
+                <div className='flex items-center justify-between gap-x-6 sm:gap-x-12 xlg:gap-x-14 xl:gap-x-24 basis-3/5 lg:basis-3/4'>
+                    <div className='flex gap-x-4 sm:gap-x-8 xlg:gap-x-12 xl:gap-x-12 dark:text-white text-sm xsm:text-base sm:text-lg'>
                         <Link className={`${dashboardboldFont}`} to={'/dashboard'}>Dashboard</Link>
                         <Link className={`${menuboldFont}`} to={'/menu'}>Menu</Link>
                     </div>
-                    <div className='flex items-center gap-x-8 xlg:gap-x-12 xl:gap-x-12'>
+                    <div className='flex items-center gap-x-4 sm:gap-x-8 xlg:gap-x-12 xl:gap-x-12'>
                         <div className='relative'>
                             <BiSolidCart className='text-primary text-2xl relative' />
                             <div className='bg-secondary rounded-full px-1 absolute -top-2 -right-2'>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,7 +3,7 @@ import Dashboardnav from '../components/dashboardComponents/dashboardnav'
 
 export default function Dashboard() {
   return (
-    <div className='dark:bg-darkModeBlack px-16'>
+    <div className='dark:bg-darkModeBlack px-4 xsm:px-8 sm:px-12 xlg:px-24'>
       <Dashboardnav dashboardboldFont={'font-semibold'} menuboldFont={''} />
     </div>
   )
